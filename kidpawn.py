188c048081241d1909c66770734d0de107a13847
Leo Dirac <leo@positronix.ai>
2020-01-28 21:29:37 -0800

    Score function considers game-ending moves.

--- kidpawn.py
+++ kidpawn.py
@@ -4,12 +4,6 @@ import chess
 from chess import Board, Move
 
 
-def score_move(b:Board, m:Move) -> int:
-    b.push(m)
-    s = score_material(b)
-    b.pop()
-    return s
-
 def score_material(b:Board) -> int:
     """Returns sum(white) - sum(black)
     """
@@ -27,9 +21,31 @@ def score_material(b:Board) -> int:
             num_p = len(p)
             total += sign * num_p * strengths[piece_type]
     return total
-    
 
-def pick_move(b:Board, score_func=score_move) -> Move:
+
+def score_material_and_win(b:Board) -> int:
+    if b.is_game_over():
+        res = b.result()
+        if res == '1/2-1/2':
+            return 0
+        elif res == '1-0':
+            return 9999
+        elif res == '0-1':
+            return -9999
+        else:
+            raise RuntimeException(f"Unknown result {res}")
+    else:
+        return score_material(b)
+    
+def score_move(b:Board, m:Move, score_func) -> int:
+    b.push(m)
+    try:
+        score = score_func(b)
+        return score
+    finally:
+        b.pop()
+
+def pick_move(b:Board, score_func=score_material_and_win, verbose:bool=False) -> Move:
     best_score = None
     best_moves = None
     def better(reference_score, new_score) -> bool:
@@ -40,15 +56,17 @@ def pick_move(b:Board, score_func=score_move) -> Move:
         else:
             return reference_score > new_score
     for move in b.legal_moves:
-        score = score_func(b, move)
-        #print(f"{move} gives {score}. ", end='')
+        score = score_move(b, move, score_func)
+        if verbose:
+            print(f"{move} gives {score}.")
         if better(best_score, score):
             best_score = score
             best_moves = [move]
         elif score == best_score:
             best_moves.append(move)
 
-    #print(f"\nBest moves are {best_moves} scoring {best_score}")
+    if verbose:
+        print(f"\nBest moves are {best_moves} scoring {best_score}")
     return random.choice(best_moves)
 
 
@@ -60,5 +78,8 @@ def self_play(b:Board):
         print(f"\nMaking move {m}")
         b.push(m)
         print(b)
-    print("\n\nGame Over")
+
+    result = b.result()
+    print("\n\nGame Over: {result}")
+    return result
 
