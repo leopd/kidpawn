5de4fd757c15b921b13a9e34f28f80e27b0c7b49
Leo Dirac <leo.dirac@gmail.com>
2020-02-11 08:42:50 -0800

    comments on jquery.

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 <script>
+// Simple jquery program so that when you click a square,
+// it puts the name of the square in the "movebox" input field.
 $(function() {
     function square_from_class(classes_str) {
-        // SVG squares
+        // This takes a list of classes for an SVG square like
+        // "square light g2" and parses out just "g2"
         var classes_list = classes_str.split(' ');
         for (var i in classes_list) {
             if (classes_list[i].length == 2) {
